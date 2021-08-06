a5e3e3428977dd1bb2dacd23d6d1b9fc3bb195ae
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-08-06 17:17:19 -0500

    Expand on .action in packages/src/commands/serve.ts

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -5,6 +5,6 @@ export const serveCommand = new Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run server on', '4005')
-    .action((filename = 'notebook.js', options) => {
-        serve(options.port)
+    .action((filename = 'notebook.js', options: { port: string}) => {
+        serve(parseInt(options.port), filename, '/')
     })
